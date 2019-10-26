a63137f1b0911a03dd947fdccef1ec9dfed38c9a
Aakash Sudhakar <aakash.sudhakar@gmail.com>
2019-10-25 17:53:10 -0700

    [ADD] Pushing Module 2.9

---
+++ 2-core-programming-principles/2-9-types_of_variables.R
@@ -0,0 +1,33 @@
+# Declare integer type as variable `x`
+# NOTE: The operator `L` denotes the value of 2 as an integer
+# By default (without `L`), 2 would have been saved as an double
+x <- 2L
+
+# Check type of variable `x`
+typeof(x)
+
+# Declare float/double type as variable `y`
+y <- 2.5
+
+# Check type of variable `y`
+typeof(y)
+
+# Declare complex type as variable `z`
+z <- 3 + 2i
+
+# Check type of variable `z`
+typeof(z)
+
+# Declare character type as variable `a`
+a <- "h"
+
+# Check type of variable `a`
+typeof(a)
+
+# Declare logical type as variables `q1` and `q2`
+q1 <- T
+q2 <- TRUE
+
+# Check types of variables `q1` and `q2`
+typeof(q1)
+typeof(q2)
