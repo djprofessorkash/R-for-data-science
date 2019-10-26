263fbb0909220012173c5a2df936391eb01a6a2e
Aakash Sudhakar <aakash.sudhakar@gmail.com>
2019-10-25 18:13:34 -0700

    [ADD] Pushing Module 10: Using Variables

---
+++ 2-core-programming-principles/2-10-using-variables.R
@@ -0,0 +1,35 @@
+# Declare two doubles `A` and `B` to setup variable addition
+A <- 10
+B <- 5
+
+# Add two variables and assign result to third variable `C`
+C <- A + B
+
+# Print value of variable `C` to console
+C
+
+# Subtract two variables and assign result to third variable `D`
+D <- A - B
+D
+
+# Declare two doubles `var1` and `var2` to setup variable division
+var1 <- 2.5
+var2 <- 4
+
+# Divide two variables and assign result to third variable `result_div` and print
+result_div <- var1 / var2
+result_div
+
+# Multiply two variables and assign result to variable `result_prod` and print
+result_prod <- var1 * var2
+result_prod
+
+# Attain square root of `var2` and assign result to variable `answer` and print
+answer <- sqrt(var2)
+answer
+
+# Create concatenated message string and print to console
+greeting <- "Hello"
+name <- "Bob"
+message <- paste(greeting, name)
+message
