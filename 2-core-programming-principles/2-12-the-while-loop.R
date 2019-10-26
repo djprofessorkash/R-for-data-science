477f6fd382dfb560e8b5701acbc57415cb6d0961
Aakash Sudhakar <aakash.sudhakar@gmail.com>
2019-10-25 18:29:22 -0700

    [ADD] Pushing Module 12: The 'While' Loop

---
+++ 2-core-programming-principles/2-12-the-while-loop.R
@@ -0,0 +1,16 @@
+# Utilize `while` loop for infinite looping
+while(TRUE){
+    print("Hello")
+}
+
+# Utilize `while` loop for unsuccessful looping
+while(FALSE){
+    print("Goodbye")
+}
+
+# Utilize `while` loop for finite looping using counters
+counter <- 1
+while(counter < 12){
+    print(counter)
+    counter <- counter + 1
+}
