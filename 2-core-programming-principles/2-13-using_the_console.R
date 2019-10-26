b505ea6b066517ca9dd4e3411f86bbb75bfd8bef
Aakash Sudhakar <aakash.sudhakar@gmail.com>
2019-10-25 18:34:43 -0700

    [ADD] Pushing Module 13: Using the Console

---
+++ 2-core-programming-principles/2-13-using_the_console.R
@@ -0,0 +1,8 @@
+# Printing a variable's value to console
+# NOTE: The console allows our environment to be responsive for developing in R
+x <- 5
+x
+
+# Write the following into the console to test its interactions with R-Studio
+# y <- 4
+# y
